438212ec502ff5fce97904125ddc1f0af8f404fe
PrKsushka <ksushkaprot@gmail.com>
2022-04-30 22:59:52 +0300

    changes

--- client/src/api/dataAboutPlaces.ts
+++ client/src/api/dataAboutPlaces.ts
@@ -1,5 +1,3 @@
 import axios from 'axios';
 
-export const sortPlacesByCategory = (value: string) => axios.get(`http://localhost:3001/categoryPlaces?includePlaces=true&placesCategory=${value}`);
-
 export const getDataAboutPlaces = () => axios.get('http://localhost:3001/places');
