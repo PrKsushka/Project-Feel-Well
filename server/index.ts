78bbd52bf5133de36a3be4efe9f2ea01ecc75b67
PrKsushka <ksushkaprot@gmail.com>
2022-05-06 20:02:13 +0300

    create basic postgres configuration

--- server/index.ts
+++ server/index.ts
@@ -8,20 +8,28 @@ import getDataAboutPlacesMongoDB from './db/routes/places/placesRoutes';
 import cors from 'cors';
 import userRoute from './db/routes/user/userRoutes';
 import getDataAboutCategoriesMongoDB from './db/routes/category/categoryRoutes';
+import "reflect-metadata"
+import connectionToPostgresDataBase from './db/postgres/db';
 
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-connectionToMongoDataBase();
 app.use(cors());
-app.use(getDataAboutProductsMongoDB);
-app.use(getDataAboutArticlesMongoDB);
-app.use(getDataAboutCategoriesMongoDB);
+
+if(process.argv[2]==='mongodb'){
+  connectionToMongoDataBase();
+  app.use(getDataAboutProductsMongoDB);
+  app.use(getDataAboutArticlesMongoDB);
+  app.use(getDataAboutCategoriesMongoDB);
 // app.use(getDataAboutCategoriesPlacesMongoDB);
-app.use(getDataAboutMealMongoDB);
-app.use(getDataAboutPlacesMongoDB);
-app.use(userRoute);
+  app.use(getDataAboutMealMongoDB);
+  app.use(getDataAboutPlacesMongoDB);
+  app.use(userRoute);
+}
+else if(process.argv[2]==='postgres'){
+  connectionToPostgresDataBase();
+}
+
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${process.env.PORT}`);
 });
